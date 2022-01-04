ced4e01e217d861ba74f4b9b5115d9e5daff59a4
Vincent <vincent.takahashi@yahoo.com>
2022-01-04 14:43:35 +0100

    tour de jeu v2 avec mode de jeu

--- main.py
+++ main.py
@@ -2,32 +2,80 @@
 
 import chess
 
-if 'name' == '__main__' :
 
-    board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
+if __name__ == '__main__' :
+
+    board = chess.Board()
     print(board)
     numero_tour_joueur = 0
     coup_joueur_blanc = None
     coup_joueur_noir = None
+    mode_jcj = 0
+    joueur_est_blanc = 1
+    coup_joueur = None
+    choix_mode = None
+
+    while choix_mode != 'y' and choix_mode != 'n' :
+        print('Etes vous deux joueurs ? y/n')
+        choix_mode = input()
+        if choix_mode == 'y' :
+            mode_jcj = 1 
+        elif choix_mode == 'n' : 
+            mode_jcj = 0
 
 
-    while board.is_checkmate() == False :
+
+    if mode_jcj == 1 :
+        while board.is_checkmate() == False :
     
-        if numero_tour_joueur%2 == 0 :
-            print("C'est au tour des blancs")
-            input(coup_joueur_blanc)
-            board.push(coup_joueur_blanc)
-        else : 
-            print("C'est au tour des noir")
-            input(coup_joueur_noir)
-            board.push(coup_joueur_noir)
+            if numero_tour_joueur%2 == 0 :
+                print("C'est au tour des blancs")
+                input(coup_joueur_blanc)
+                board.push(coup_joueur_blanc)
+            else : 
+                print("C'est au tour des noir")
+                input(coup_joueur_noir)
+                board.push(coup_joueur_noir)
 
-        i = i+1
+            i = i+1
 
-    if i % 2 == 1 :
-        print('victoire du joueur noir')
-    else  :
-        print('victoire du joueur blanc')
+        if i % 2 == 1 :
+            print('victoire du joueur noir')
+        else  :
+            print('victoire du joueur blanc')
+
+    else :
+        if joueur_est_blanc == 1 :
+            
+            while board.is_checkmate() == False :
+    
+                if numero_tour_joueur%2 == 0 :
+                    print("A votre tour")
+                    input(coup_joueur)
+                    board.push(coup_joueur)
+                else : 
+                    pass
+                    #coup IA a completer
+                
+
+                i = i+1
+
+            if i % 2 == 1 :
+                print('victoire du joueur noir')
+            else  :
+                print('victoire du joueur blanc')
+        else :
+            while board.is_checkmate() == False :
+
+                if numero_tour_joueur%2 == 1 :
+                    print ("A votre tour")
+                    input(coup_joueur)
+                    board.push(coup_joueur)
+                else :
+                    pass
+                    # coup IA à compléter
+
+        
 
 
 
