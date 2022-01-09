5412b1fe20997960036ba60f76e16312ea36e1df
theBigBlase <thebigblase@gmail.com>
2022-01-09 17:50:53 +0100

    a lot has changed, should be working

--- src/board/board_and_movment.py
+++ src/board/board_and_movment.py
@@ -1,62 +1,143 @@
 import chess
+from choixMode.choixMode import choixMode
+import tourDeJeu.tourDeJeu as tdj
 
-board = chess.Board()
-#board de base : chess.Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
+class PolyBoard:
 
-board.legal_moves   # Met en oeuvre et vérifie si le mouvement qui est effectué est légal ou non
-
-def traduction(string):
-    if string == "1" :
-        return str('.')
-    if string == "2" :
-        return str('. .')
-    if string == "3" :
-        return str('. . .')
-    if string == "4" :
-        return str('. . . .')
-    if string == "5" :
-        return str('. . . . .')
-    if string == "6" :
-        return str('. . . . . .')
-    if string == "7" :
-        return str('. . . . . . .')
-    if string == "8" :
-        return str('. . . . . . . .')
-    else : return string
+    def __init__(self):
+        self.board = chess.Board()
+        #board de base : chess.Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
+        self.printBanner()
+        self.board.legal_moves   #??????????
+        self.mode = choixMode()#TODO
+        self.WHITE = chess.WHITE
+        self.BLACK = chess.BLACK
 
 
-def affichage_plateau(board):
-    copie = board.epd()
-    compteur = 0
-    ch = ""
-    #print("    A B C D E F G H ")
-    print("   ___________________")
-    print("  |                   |")
-    for j in copie :
-        if j != "/" :
-            ch += traduction(j) + " "
-        else : 
-            print(8 - compteur, '| ', ch, '|', 8 - compteur)
-            compteur += 1
-            ch = ""
-    print("1", '| ', ch[0:16], '|', "1")
-    print("  |___________________|")
-    print("     A B C D E F G H")
-  
-print (affichage_plateau(board))
+    def traduction(self, string): #pas besoin de str-ify une str
+        if string == "1" :
+            return str('.')
+        if string == "2" :
+            return str('. .')
+        if string == "3" :
+            return str('. . .')
+        if string == "4" :
+            return str('. . . .')
+        if string == "5" :
+            return str('. . . . .')
+        if string == "6" :
+            return str('. . . . . .')
+        if string == "7" :
+            return str('. . . . . . .')
+        if string == "8" :
+            return str('. . . . . . . .')
+        else : return string
 
-def detect_echec():
-    if board.is_checkmate() == False and board.is_check() == False:
-        print('La partie continue')
-    elif board.is_checkmate() == False and board.is_check() == True:
-        print('Echec')
-    elif board.is_checkmate() == True:
-        print('Echec et Mat')
-             
 
-def move_piece(str):
-    #str est du type "'case de départ' 'case d'arrivée'" (ex: 'e2e3' bouge la piece de e2 à e3)
-    board.push_san(str)
-    affichage_plateau(board)
-    print(affichage_plateau(board))
-    detect_echec()
+    def affichage_plateau(self):
+        copie = self.board.epd()
+        compteur = 0
+        ch = ""
+        #print("    A B C D E F G H ")
+        print("   ___________________")
+        print("  |                   |")
+        for j in copie :
+            if j != "/" :
+                ch += self.traduction(j) + " "
+            else : 
+                print(8 - compteur, '| ', ch, '|', 8 - compteur)
+                compteur += 1
+                ch = ""
+        print("1", '| ', ch[0:16], '|', "1")
+        print("  |___________________|")
+        print("     A B C D E F G H")
+      
+
+    def detect_echec(self):
+        #if self.board.is_checkmate() == False and self.board.is_check() == False: #DO nothing so rm 
+        #    print('La partie continue')
+        if self.board.is_checkmate() == False and self.board.is_check() == True:
+            print('Echec')
+            return self.endGame()
+        elif self.board.is_checkmate() == True:
+            print('Echec et Mat')
+            return self.endGame()
+        elif self.board.is_stalemate():
+            print('Pâtes (jsp comment ca s\'ecrit')
+            return self.endGame()
+
+                 
+    def ask_move_piece(self):
+        #ask to move a piece and returns it once the move is legal
+        print("enter a move, please (undo to undo)")
+        askAgain = False
+        coup = "0000"
+        try:
+            while(chess.Move.from_uci(coup) not in self.board.legal_moves): #NOTE maybe put it in another func, moveDetect ?
+                if askAgain: 
+                    print("Ce coup n'est pas valide ! \n les coups valides sont : ")
+                    print(self.board.legal_moves)
+                coup = input()
+                print(coup)
+                askAgain = True
+                if coup == "undo" and self.board.fullmove_number > 1: return "undo"
+        except ValueError: #hacky way to do things, but it works
+            print("Ce coup n'est pas valide ! \n recommencez : ")
+            return self.ask_move_piece()
+        return coup
+
+
+    def move_piece(self):
+        #x est du type "'case de départ' 'case d'arrivée'" (ex: 'e2e3' bouge la piece de e2 à e3)
+        #pour roque, il suffit de bouger le roi sur la case ou est presente une tou 
+        coup = self.ask_move_piece()
+        if coup == "undo": self.coup_precedent()
+        self.board.push_san(coup)
+        print(self.affichage_plateau())
+        self.detect_echec()
+
+
+    def coup_precedent(self):
+        self.board.pop()
+        print(self.affichage_plateau())
+        self.detect_echec()
+
+           
+    def endGame(self):
+        term = self.board.outcome().winner
+        if term: term = "blancs"
+        elif term: term = "noirs"
+        elif term == None: return print("Pat ! GG WP !")
+        return print(f"Les {term} ont gagnés ! GGWP !")
+
+
+    def getTurn(self):
+        return self.board.turn
+
+
+    def isGameFinished(self): 
+        if self.board.outcome() == None: return False
+        return True
+
+    def playGame(self):
+        while not self.isGameFinished():
+            assert self.board.is_valid()
+            self.affichage_plateau()
+
+            tdj.tourJeu(self)
+
+    def printBanner(self):
+        print("")
+        print("     ██████╗  ██████╗ ██╗     ██╗   ██╗ ██████╗██╗  ██╗███████╗███████╗███████╗")
+        print("     ██╔══██╗██╔═══██╗██║     ╚██╗ ██╔╝██╔════╝██║  ██║██╔════╝██╔════╝██╔════╝")
+        print("     ██████╔╝██║   ██║██║      ╚████╔╝ ██║     ███████║█████╗  ███████╗███████╗")
+        print("     ██╔═══╝ ██║   ██║██║       ╚██╔╝  ██║     ██╔══██║██╔══╝  ╚════██║╚════██║")
+        print("     ██║     ╚██████╔╝███████╗   ██║   ╚██████╗██║  ██║███████╗███████║███████║")
+        print("     ╚═╝      ╚═════╝ ╚══════╝   ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝")
+        print("Bienvenu !")
+            
+
+    '''def mvt_possible(self, str):
+        self.board.find_move(from_square: chess.str)
+        print(affichage_plateau(self.board))
+        detect_echec()'''
