9f752a305fa0bd461d4690d08b701a3cb49eae33
theBigBlase <thebigblase@gmail.com>
2022-01-13 09:33:49 +0100

    fixx

--- src/evalBar/evalBar.py
+++ src/evalBar/evalBar.py
@@ -8,9 +8,9 @@ class Eval:
     def __init__(self):
         if platform.system() == 'Windows':
             self.engine = chess.engine.SimpleEngine.popen_uci(os.path.abspath(\
-                "./src/evalBar//stockfish_14.1_win_x64_popcnt/stockfish_14.1_win_x64_popcnt.exe")) #Prayge
+                "./evalBar//stockfish_14.1_win_x64_popcnt/stockfish_14.1_win_x64_popcnt.exe")) #Prayge
         else: #mac and linux, regex match cuz not same location for at least arch, mac and ubuntu
-            res = subprocess.check_output(['whereis', 'stockfish']).decode("utf-8")
+            res = subprocess.check_output(['which', 'stockfish']).decode("utf-8")
             res = re.findall(r'/usr/[a-z\/]*/stockfish', res)
             self.engine = chess.engine.SimpleEngine.popen_uci(res)
 
